58d9357c9eef2ff1575b7727cb8ad80b5b73000d
BACHTIAR8987854 <aminudinbahtiar101@gmail.com>
2021-03-14 01:15:44 +0700

    menemukan error di container

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,31 @@
+@extends('contact')
+@section('main')
+
+    <div class="row">
+        <div class="col-md-12">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>    
+@endsection
